8f0489d7756cfb0f498993269446d7c0eefd20a6
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-10 19:22:00 -0400

    sync remote with local

---
+++ assets/js/logic.js
@@ -0,0 +1,29 @@
+const api = `api.openweathermap.org/data/2.5/forecast?q=${city name}&appid=${API key}`
+
+fetch(locQueryUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+
+      return response.json();
+    })
+    .then(function (locRes) {
+      // write query to page so user knows what they are viewing
+      resultTextEl.textContent = locRes.search.query;
+
+      console.log(locRes);
+
+      if (!locRes.results.length) {
+        console.log('No results found!');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        resultContentEl.textContent = '';
+        for (let i = 0; i < locRes.results.length; i++) {
+          printResults(locRes.results[i]);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
