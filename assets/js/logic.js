2dd9413bad5fcfa73ebf8f400cf6fbde57f8be3a
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-10 20:58:39 -0400

    added localStorage function for cities array

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,8 @@
-// const apiKey = 'af1a16eb878a4e1e1b04e04dee962c30';
-const apiKey = '166a433c57516f51dfab1f7edaed8413';
+const apiKey = 'af1a16eb878a4e1e1b04e04dee962c30';
 const searchInput = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-btn');
 const containerEl = document.getElementById('container');
+const storedCities = JSON.parse(localStorage.getItem('cities')) || [];
 
 function getData() {
   let city = searchInput.value.trim()
@@ -15,6 +15,7 @@ function getData() {
   })
   .then(function (data) {
     renderCurrentWeather(city, data);
+    storeSearchHistory(city);
     console.log(data);
     
   })
@@ -31,9 +32,11 @@ function renderCurrentWeather(city, weather) {
   const temp = weather.list[0].main.temp;
   const humid = weather.list[0].main.humidity;
   const wind = weather.list[0].wind.speed;
+  const date = dayjs().format('D/MM/YYYY')
 
   // create elements to render values to
   const cityEl = document.createElement('h1')
+  const dateEl = document.createElement('h1')
   const tempEl = document.createElement('h1')
   const humidEl = document.createElement('h1')
   const windEl = document.createElement('h1')
@@ -41,14 +44,20 @@ function renderCurrentWeather(city, weather) {
 
   // insert values to elements
   cityEl.textContent = city;
+  dateEl.textContent = `(${date})`;
   iconEl.setAttribute('src', iconUrl)
   tempEl.textContent = temp;
   humidEl.textContent = humid;
   windEl.textContent = wind;
   
   // append elements to the page
-  containerEl.append(cityEl, iconEl, tempEl, humidEl, windEl);
+  containerEl.append(cityEl, dateEl, iconEl, tempEl, humidEl, windEl);
 
+}
+
+function storeSearchHistory(city) {
+  storedCities.push(city);
+  localStorage.setItem('cities', JSON.stringify(storedCities));
 
 }
 
