844c7910104c15a1f1b513f3265d4e3e9c0b53b5
justmacn <172618812+justmacn@users.noreply.github.com>
2024-07-10 20:20:17 -0400

    added getData and renderWeather functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,37 @@
-const api = `api.openweathermap.org/data/2.5/forecast?q=${city name}&appid=${API key}`
+// const apiKey = 'af1a16eb878a4e1e1b04e04dee962c30';
+const apiKey = '166a433c57516f51dfab1f7edaed8413';
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
 
-fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
+function getData() {
+  let city = searchInput.value.trim()
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
-      return response.json();
-    })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
+  fetch(apiUrl)
+  .then(function (response) {
+    return response.json();
+    
+  })
+  .then(function (data) {
+    renderCurrentWeather(city, data);
+    console.log(data);
+    
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
-      console.log(locRes);
+}
 
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (let i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+function renderCurrentWeather(city, weather) {
+  const date = null;
+  const icon = weather.list[0].weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`
+  const temp = weather.list[0].main.temp;
+  const humid = weather.list[0].main.humidity;
+  const wind = weather.list[0].wind.speed;
+  console.log(iconUrl);
+
+}
+
+searchBtn.addEventListener('click', getData)
